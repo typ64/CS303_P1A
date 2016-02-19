5c43f3b699252d47ff1fad8cd7d70c345501a85a
typ64 <typ64@yahoo.com>
2016-02-19 17:38:32 -0600

    changed

--- CS303_P1A/CS303_P1A/main.cpp
+++ CS303_P1A/CS303_P1A/main.cpp
@@ -7,6 +7,7 @@ int main() {
 	Assignment assignment1("2-28-2016", "Linked Lists", "2-20-2016", assigned);
 	cout << assignment1.get_description() << endl;
 
+	//grits
 	system("Pause");
 	return 0;
 }

eb589bec5abf930969f616babf6d5eec29170ea8
candicelj1708 <candicelj1708@gmail.com>
2016-02-19 17:38:53 -0600

    Test test test

--- CS303_P1A/CS303_P1A/main.cpp
+++ CS303_P1A/CS303_P1A/main.cpp
@@ -6,7 +6,7 @@
 int main() {
 	Assignment assignment1("2-28-2016", "Linked Lists", "2-20-2016", assigned);
 	cout << assignment1.get_description() << endl;
-
+	//TEST TEST TEST
 	system("Pause");
 	return 0;
 }

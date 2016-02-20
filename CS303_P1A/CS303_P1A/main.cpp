77cdbbcc53cd782ecf8bc65b1cd748270c08113f
typ64 <typ64@yahoo.com>
2016-02-19 20:16:20 -0600

    ok

--- CS303_P1A/CS303_P1A/main.cpp
+++ CS303_P1A/CS303_P1A/main.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 
 int main() {
-	Assignment assignment1(string, "Linked Lists", "2-20-2016", assigned);
+	Assignment assignment1("2-25-2016", "Linked Lists", "2-20-2016", assigned);
 	cout << assignment1.get_description() << endl;
 
 	system("Pause");
